c881217d5110087e4e8a795b4a83302a949a6ac7
Alexander <alexander@Mac-mini-Mark.local>
2012-11-06 10:04:56 +0200

    Initial Commit

---
+++ Project1_DNA/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Project1_DNA
+//
+//  Created by Alexander on 06.11.12.
+//  Copyright (c) 2012 Alexander. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
