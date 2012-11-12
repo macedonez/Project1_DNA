e730f5252c0e4d7d64a0f803a460d438608ad1b7
macedonez <macedonez@bk.ru>
2012-11-12 10:46:18 +0200

    FINAL

--- Project1_DNA/main.m
+++ Project1_DNA/main.m
@@ -7,14 +7,25 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "Cell.h"
 
 int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        Cell *cell = [[Cell alloc]init];
+        Cell *cell2 = [[Cell alloc]init];
+        
+        int hammFirst = [cell hammingDistance:cell2];
+        
+        NSLog(@"%i",hammFirst);
+        
+        [cell mutator:60];
+        [cell2 mutator:60];
+        
+        int hammSecond = [cell hammingDistance:cell2];
+        NSLog(@"%i",hammSecond);
         
     }
     return 0;
